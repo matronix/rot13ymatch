5deb94e27ee6a99281e1f7317b48fe9d45acfa8f
fathemancre <ymatcheswala@gmail.com>
2016-03-18 00:09:14 -0400

    experimenting and making notes on various ways of using get and post. Specifically various ways of redirect using form redirect by action attr and using self.redirect and the differences between these

--- main.py
+++ main.py
@@ -15,11 +15,14 @@
 # limitations under the License.
 #
 import webapp2
-import cgi
 import string
+import os
+import jinja2
 
-def escape_html(s):
-    return cgi.escape(s, quote=True)
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
 
 letters = list(string.ascii_lowercase)
 #Given a letter, returns its rot13 complement adjusting for index >25
@@ -51,33 +54,49 @@ def rot13Encode(s):
     return resultString
 
 
-form="""
-<h2>Enter some text to ROT13:</h2>
-<form method="post">
-      <textarea name="text" style="height: 100px; width: 400px;">%(encodedString)s</textarea>
-      <br>
-      <input type="submit">
-    </form>
-"""
-
-
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self, encodedString=''):
-        self.response.out.write(form%({'encodedString':escape_html(encodedString)}))
+    
+    def render(self, template, **kwargs):
+        t = jinja_env.get_template(template)
+        self.response.out.write(t.render(kwargs))
 
+    #when browser sends a get request, the get method responds with
+    #the required document. This generally has a response object to
+    #provide browser with HTML to be rendered. If the request has parameters
+    #supplied in the for ?param='someValue' then a self.request.get line might
+    #be defined to handle that request
+    #Remember, browser makes a GET REQUEST and server RESPONDS
+    #with a GET RESPONSE, which is usually HTML
+    def get(self, encodedString=''):
+        self.render('rot13-form.html', text='')
+                                
+
+    #def post(self):
+        #encode the typed string that is accessed by
+        #self.request.get('text')
+        #encodedString=rot13Encode(self.request.get('text'))
+        #pass this encoded text back to the textarea
+        #self.render('rot13-form.html', text=encodedString)
+        #self.redirect('/output?encodedString='+encodedString)
+
+class Rot13Handler(MainHandler):
     def post(self):
         #encode the typed string that is accessed by
         #self.request.get('text')
-        encodedString=rot13Encode(self.request.get('text'))
-        #pass encodedString through the escape_html filter to escape
-        #html and other special chars
-        #encodedString = escape_html(encodedString)
+        #encodedString=rot13Encode(self.request.get('text'))
         #pass this encoded text back to the textarea
-        self.response.out.write(form%({'encodedString':escape_html(encodedString)}))
-#        self.response.out.write(encodedString)
-        
+        #self.render('rot13-form.html', text=encodedString)
+        encodedString = rot13Encode(self.request.get('text'))
+        self.response.out.write(encodedString)
+        self.response.out.write(self.response)
+
+ #In this example, instead of redirecting which requires that the parameter be
+#passed in the URL, I am setting the redirect target by using the "action" attribute in the form
+        #tag and then defining post method in the handler for the "action" attr. The post
+        #method use the request.get to obtain the text from the <textfield>, encodes it
+        #and then send response by response.out.write to browser to displat the text
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler), ('/output', Rot13Handler)
 ], debug=True)
